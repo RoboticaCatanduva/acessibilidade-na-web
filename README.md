9cd63ec0f79f5d1c970ddced6c29407737d0bd89
RoboticaCatanduva <roboticacatanduva@gmail.com>
2025-09-16 14:17:52 -0300

    Aula 10

---
+++ README.md
@@ -0,0 +1,14 @@
+# Site acessível sobre tropicália
+# Sobre
+Refatoração de um site implementando recursos de acessibilidade no HTML, CSS e JS
+## Recursos de acessibilidade
+- Atributos Aria
+- Alt
+- Tab-index
+- Menu de acessibilidade
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollRevealjs
+- HTML
+- CSS
+- JS
--- script.js
+++ script.js
@@ -5,11 +5,17 @@ document.addEventListener('DOMContentLoaded', function(){
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
     })
     
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste');
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -18,4 +24,13 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
+    })
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
